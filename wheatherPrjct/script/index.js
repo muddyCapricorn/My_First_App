164d100b5dc3acc660d89d1c5fd98baa9deb3cab
muddyCapricorn <zubaklesya@gmail.com>
2022-07-06 11:15:37 +0300

    Added my project to GitHub

---
+++ wheatherPrjct/script/index.js
@@ -0,0 +1,88 @@
+let newDate = new Date();
+let weekend = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+let dayWeekend = weekend[newDate.getDay()];
+function currentM() {
+  let m = newDate.getMinutes();
+  if (m < 10) {
+    return `0${m}`;
+  } else {
+    return m;
+  }
+}
+let minute = currentM();
+
+let time = `${newDate.getHours()}:${minute}`;
+
+let today = document.querySelector(".today");
+today.innerHTML = `${dayWeekend}  <br/> ${time}`;
+
+function showWeather(response) {
+  document.querySelector("h1").innerHTML = response.data.name;
+  document.querySelector("#temp-now").innerHTML = `${Math.round(
+    response.data.main.temp
+  )}°`;
+  document.querySelector(".descr").innerHTML =
+    response.data.weather[0].description;
+  document.querySelector(
+    "#humidity"
+  ).innerHTML = `${response.data.main.humidity}%`;
+  document.querySelector("#wind").innerHTML = `${Math.round(
+    response.data.wind.speed
+  )} km/h`;
+  //document.querySelector("#min-today").innerHTML = `${Math.round(
+  //response.data.main.temp_min
+  //)}°`;
+
+  //document.querySelector("#max-today").innerHTML = `/${Math.round(
+  // response.data.main.temp_max
+  //)}°`;
+}
+function searchCity(city) {
+  let apiKey = "0a016ab1d0d641ea3159c73766f38d70";
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric`;
+  axios.get(`${url}&appid=${apiKey}`).then(showWeather);
+}
+
+function changeCity(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-city").value;
+  searchCity(city);
+}
+
+let form = document.querySelector("form");
+form.addEventListener("submit", changeCity);
+
+function showLocalPosition(position) {
+  let apiKey = "0a016ab1d0d641ea3159c73766f38d70";
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let locUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric`;
+  axios.get(`${locUrl}&appid=${apiKey}`).then(showWeather);
+}
+function localPosition() {
+  navigator.geolocation.getCurrentPosition(showLocalPosition);
+}
+
+let locButton = document.querySelector("#location-button");
+locButton.addEventListener("click", localPosition);
+
+function convertFahr(event) {
+  event.preventDefault();
+  let now = document.querySelector("#temp-now°");
+  let fahr = Math.round((now * 9) / 5 + 32);
+  now.innerHTML = `☀️ ${fahr}°`;
+}
+function convertCel(event) {
+  event.preventDefault();
+  let now = document.querySelector("#now°");
+  let cel = 18;
+  now.innerHTML = `☀️ ${cel}°`;
+}
+let celConv = document.querySelector("#celsium a");
+let fahrConv = document.querySelector("#fahrenheit a");
+
+celConv.addEventListener("click", convertCel);
+fahrConv.addEventListener("click", convertFahr);
+
+searchCity("Kyiv");
