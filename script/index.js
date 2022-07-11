ffb1742b03d1e97bdcdcb7b7fdc77411f6cdfc99
muddyCapricorn <zubaklesya@gmail.com>
2022-07-11 22:33:22 +0300

    Updated units conversion js and css

--- script/index.js
+++ script/index.js
@@ -1,35 +1,38 @@
 function formatDate(timestamp) {
   let newDate = new Date(timestamp);
   let weekend = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-
   let dayWeekend = weekend[newDate.getDay()];
-
   let m = newDate.getMinutes();
   if (m < 10) {
     m = `0${m}`;
   }
 
   let time = `${newDate.getHours()}:${m}`;
-
   return `${dayWeekend}  <br/> ${time}`;
 }
 
 function showWeather(response) {
   document.querySelector("h1").innerHTML = response.data.name;
-  document.querySelector("#temp-now").innerHTML = `${Math.round(
-    response.data.main.temp
-  )}°`;
+
+  let tempNow = document.querySelector("#temp-now");
+
+  celTemp = response.data.main.temp;
+  tempNow.innerHTML = Math.round(celTemp);
   document.querySelector(".descr").innerHTML =
     response.data.weather[0].description;
+
   document.querySelector(
     "#humidity"
   ).innerHTML = `${response.data.main.humidity}%`;
+
   document.querySelector("#wind").innerHTML = `${Math.round(
-    response.data.wind.speed
+    response.data.wind.speed * 3.6
   )} km/h`;
+
   document.querySelector(".today").innerHTML = formatDate(
     response.data.dt * 1000
   );
+
   document
     .querySelector("#emoji-now")
     .setAttribute(
@@ -50,9 +53,6 @@ function changeCity(event) {
   searchCity(city);
 }
 
-let form = document.querySelector("form");
-form.addEventListener("submit", changeCity);
-
 function showLocalPosition(position) {
   let apiKey = "0a016ab1d0d641ea3159c73766f38d70";
   let lat = position.coords.latitude;
@@ -64,25 +64,32 @@ function localPosition() {
   navigator.geolocation.getCurrentPosition(showLocalPosition);
 }
 
-let locButton = document.querySelector("#location-button");
-locButton.addEventListener("click", localPosition);
-
 function convertFahr(event) {
   event.preventDefault();
-  let now = document.querySelector("#temp-now°");
-  let fahr = Math.round((now * 9) / 5 + 32);
-  now.innerHTML = `☀️ ${fahr}°`;
+  celConv.classList.remove("active");
+  fahrConv.classList.add("active");
+  let now = document.querySelector("#temp-now");
+  let fahr = Math.round((celTemp * 9) / 5 + 32);
+  now.innerHTML = fahr;
 }
 function convertCel(event) {
   event.preventDefault();
-  let now = document.querySelector("#now°");
-  let cel = 18;
-  now.innerHTML = `☀️ ${cel}°`;
+  fahrConv.classList.remove("active");
+  celConv.classList.add("active");
+  let now = document.querySelector("#temp-now");
+  now.innerHTML = Math.round(celTemp);
 }
+let celTemp = null;
+let form = document.querySelector("form");
+form.addEventListener("submit", changeCity);
+
+let locButton = document.querySelector("#location-button");
+locButton.addEventListener("click", localPosition);
+
 let celConv = document.querySelector("#celsium a");
 let fahrConv = document.querySelector("#fahrenheit a");
 
-celConv.addEventListener("click", convertCel);
 fahrConv.addEventListener("click", convertFahr);
+celConv.addEventListener("click", convertCel);
 
 searchCity("Kyiv");
